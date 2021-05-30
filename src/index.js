134f2dbf3dae47afe64eeb5aa5a0740952b92b13
Enrique Ortuno <enrique@ortuno.net>
2021-05-30 12:25:40 -0400

    json db created

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import Template from './templates/Template.js';
-console.log('hola');
+import Template from "./templates/Template.js";
+import db from "./db.json";
+
+console.log("hola", db);
 
 (async function App() {
-  const main = null || document.getElementById('main');
+  const main = null || document.getElementById("main");
   main.innerHTML = await Template();
 })();

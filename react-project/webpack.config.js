3f868889cd4ad8826758a6f090b2b31cc16c19d7
enrique.ortuno <enrique@ortuno.net>
2021-11-02 21:52:10 -0400

    webpack and config added

---
+++ react-project/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname, "dist"),
+      },
+      {
+        directory: path.join(__dirname, "src/assets"),
+        publicPath: "/src/assets",
+      },
+    ],
+    compress: true,
+    historyApiFallback: true,
+    port: 3006,
+  },
+};

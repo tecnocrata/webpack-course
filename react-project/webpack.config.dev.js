a1a682544a8317c094a60694f63aade8895f6ccc
enrique.ortuno <enrique@ortuno.net>
2021-11-03 22:14:00 -0400

    optimizations for production mode

--- react-project/webpack.config.dev.js
+++ react-project/webpack.config.dev.js
@@ -1,24 +1,16 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CSSMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin"); // not required install into package.json
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "/",
   },
-  mode: "production",
+  mode: "development",
   resolve: {
     extensions: [".js", ".jsx"],
-    alias: {
-      "@components": path.resolve(__dirname, "src/component/"),
-      "@styles": path.resolve(__dirname, "src/styles/"),
-    },
   },
   module: {
     rules: [
@@ -50,20 +42,19 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "[name].css",
     }),
-    new CleanWebpackPlugin(),
   ],
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new CSSMinimizerPlugin(),
-      new TerserPlugin({
-        terserOptions: {
-          format: {
-            comments: false,
-          },
-        },
-        extractComments: false,
-      }),
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname, "dist"),
+      },
+      {
+        directory: path.join(__dirname, "src/assets"),
+        publicPath: "/src/assets",
+      },
     ],
+    compress: true,
+    historyApiFallback: true,
+    port: 3006,
   },
 };
